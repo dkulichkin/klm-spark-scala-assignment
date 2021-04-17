8e835d590c9203674675aead750d78ffefbc164b
Dmitriy Kulichkin <dmitriy.kulichkin@nike.com>
2021-04-17 21:00:00 +0200

    wip

--- src/main/scala/com/example/Notebook.scala
+++ src/main/scala/com/example/Notebook.scala
@@ -9,14 +9,14 @@ object Notebook {
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().master("local").appName("SparkExample").getOrCreate()
-    val startDate = Timestamp.valueOf("2019-03-17 14:50:00")
-    val endDate = Timestamp.valueOf("2019-03-17 14:55:00")
+    val startDate = Timestamp.valueOf("2019-01-01 00:00:00")
+    val endDate = Timestamp.valueOf("2019-05-31 14:55:00")
     val config = UsageConfig(startDate, endDate)
     val storage = new LocalStorage(spark, config.bookingsURI, config.airportsURI)
 
     storage.bookings
-      .filter(col("timestamp") >= config.startDate && col("timestamp") <= config.endDate)
-      .sort(col("timestamp").asc)
+      .filter(col("departureDate") >= config.startDate && col("departureDate") <= config.endDate)
+      .sort(col("departureDate").asc)
       .show(100, truncate = false)
   }
 }
--- src/main/scala/com/example/PopularDestinationsJob.scala
+++ src/main/scala/com/example/PopularDestinationsJob.scala
@@ -1,20 +1,32 @@
 package com.example
+
 import com.example.storage.Storage
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col}
+import org.apache.spark.sql.expressions.{UserDefinedFunction, Window}
+import org.apache.spark.sql.functions.{col, dense_rank, udf}
 
 object PopularDestinationsJob extends BaseSparkJob {
   val jobName = "PopularDestinationsJob"
 
   override def run(spark: SparkSession, config: UsageConfig, storage: Storage): Unit = {
+    val lookupRdd = storage.airports.select("IATA", "Country").rdd
+    val lookupCountriesMap = spark.sparkContext.broadcast(
+      lookupRdd.map(x => x(0).toString).zip(lookupRdd.map(x => x(1).toString)).collectAsMap
+    )
+    def lookupCountries: UserDefinedFunction = udf((IATA: String) => lookupCountriesMap.value.get(IATA))
+
+    val windowSpec = Window
+      .partitionBy("uci", "originAirport", "destinationAirport")
+      .orderBy(col("departureDate").desc)
+
     storage.bookings
-      .filter(col("timestamp") >= config.startDate && col("timestamp") <= config.endDate)
+      .filter(col("departureDate") >= config.startDate && col("departureDate") <= config.endDate)
       .filter(col("operatingAirline") === "KL")
-      .alias("bookings")
-      .join(storage.airports, col("originAirport") === col("IATA"))
-      .withColumnRenamed("Country", "originCountry")
+      .withColumn("rank", dense_rank().over(windowSpec))
+      .filter(col("rank") === 1 && col("bookingStatus") === "CONFIRMED")
+      .withColumn("originCountry", lookupCountries(col("originAirport")))
       .filter(col("originCountry") === "Netherlands")
-      .select("bookings.*", "originCountry")
-      .show(100, truncate = false)
+      .withColumn("destinationCountry", lookupCountries(col("destinationAirport")))
+      .show(300, truncate = false)
   }
 }

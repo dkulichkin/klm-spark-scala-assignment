219c6be60013573f4a17d606c4b01eac70f2a1b6
Dmitriy Kulichkin <dmitriy.kulichkin@nike.com>
2021-04-17 22:29:57 +0200

    wip

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
 # Commercial Booking Analysis
 
-Provide detailed information on your product and how to run it here
+Building the project:
+
+```
+sbt assembly
+```
+
+Job command line parameters:
+
+```
+Usage: job config [options]
+
+  -sd, --startDate <value> Start date (dd-MM-yyyy)
+  -ed, --endDate <value>   End date (dd-MM-yyyy)
+  --bookingsURI <value>    Bookings URI (default: data/bookings/booking.json)
+  --airportsURI <value>    Airports URI (default: data/airports/airports.dat)
+```
+
+Example of running with `spark-submit` locally (time scope of the year 2019):
+
+```
+spark-submit --class com.example.PopularDestinationsJob target/scala-2.12/SparkKLMExample-assembly-0.0.1.jar --startDate 01-01-2019 --endDate 31-12-2019
+```
+
+Results (top 20):
+
+|destinationCountry  |departureSeason|departureDay|count|
+|--------------------|---------------|------------|-----|
+|United States       |Spring         |Monday      |641  |
+|France              |Spring         |Monday      |637  |
+|United Kingdom      |Spring         |Sunday      |294  |
+|Italy               |Spring         |Monday      |223  |
+|United States       |Spring         |Sunday      |221  |
+|Portugal            |Spring         |Wednesday   |208  |
+|United Kingdom      |Spring         |Monday      |160  |
+|Germany             |Spring         |Monday      |151  |
+|China               |Spring         |Sunday      |135  |
+|United States       |Spring         |Tuesday     |115  |
+|United Kingdom      |Spring         |Tuesday     |109  |
+|United Kingdom      |Spring         |Saturday    |99   |
+|Italy               |Spring         |Tuesday     |98   |
+|China               |Spring         |Monday      |94   |
+|Austria             |Spring         |Monday      |87   |
+|United Kingdom      |Spring         |Thursday    |86   |
+|Italy               |Spring         |Wednesday   |85   |
+|United Kingdom      |Spring         |Wednesday   |81   |
+|United Kingdom      |Summer         |Friday      |73   |
+|United Kingdom      |Spring         |Friday      |72   |
--- src/main/scala/com/example/PopularDestinationsJob.scala
+++ src/main/scala/com/example/PopularDestinationsJob.scala
@@ -1,9 +1,10 @@
 package com.example
 
-import com.example.storage.Storage
+import java.sql.Timestamp
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.expressions.{UserDefinedFunction, Window}
-import org.apache.spark.sql.functions.{col, dense_rank, udf}
+import org.apache.spark.sql.functions.{col, dayofweek, dense_rank, from_utc_timestamp, month, udf}
+import com.example.storage.Storage
 
 object PopularDestinationsJob extends BaseSparkJob {
   val jobName = "PopularDestinationsJob"
@@ -14,6 +15,19 @@ object PopularDestinationsJob extends BaseSparkJob {
       lookupRdd.map(x => x(0).toString).zip(lookupRdd.map(x => x(1).toString)).collectAsMap
     )
     def lookupCountries: UserDefinedFunction = udf((IATA: String) => lookupCountriesMap.value.get(IATA))
+    def getSeason: UserDefinedFunction = udf((month: Int) => {
+      val monthToSeason = Map(
+        1 -> "Winter", 2 -> "Winter", 3 -> "Spring", 4 -> "Spring", 5 -> "Spring", 6 -> "Summer",
+        7 -> "Summer", 8 -> "Summer", 9 -> "Fall", 10 -> "Fall", 11 -> "Fall", 12 -> "Winter"
+      )
+      monthToSeason.get(month)
+    })
+    def getDayOfWeek: UserDefinedFunction = udf((day: Int) => {
+      val days = Map(
+        1 -> "Sunday", 2 -> "Monday", 3 -> "Tuesday", 4 -> "Wednesday", 5 -> "Thursday", 6 -> "Friday", 7 -> "Saturday"
+      )
+      days.get(day)
+    })
 
     val windowSpec = Window
       .partitionBy("uci", "originAirport", "destinationAirport")
@@ -27,6 +41,12 @@ object PopularDestinationsJob extends BaseSparkJob {
       .withColumn("originCountry", lookupCountries(col("originAirport")))
       .filter(col("originCountry") === "Netherlands")
       .withColumn("destinationCountry", lookupCountries(col("destinationAirport")))
-      .show(300, truncate = false)
+      .withColumn("departureDateAmsterdam", from_utc_timestamp(col("departureDate"), "Europe/Amsterdam"))
+      .withColumn("departureDay", getDayOfWeek(dayofweek(col("departureDateAmsterdam"))))
+      .withColumn("departureSeason", getSeason(month(col("departureDateAmsterdam"))))
+      .groupBy("destinationCountry", "departureSeason", "departureDay")
+      .count()
+      .sort(col("count").desc)
+      .show(numRows = 100, truncate = false)
   }
 }

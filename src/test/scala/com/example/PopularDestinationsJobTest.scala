ac74b1838f34dfc51df99ec156f7ce3b2e0cfb0b
Dmitriy Kulichkin <dmitriy.kulichkin@nike.com>
2021-04-18 14:28:13 +0200

    added an example for unit test

--- src/test/scala/com/example/PopularDestinationsJobTest.scala
+++ src/test/scala/com/example/PopularDestinationsJobTest.scala
@@ -1,19 +1,47 @@
 package com.example
 
-import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import com.example.domain.Booking
+import com.holdenkarau.spark.testing.DatasetSuiteBase
+import org.apache.spark.sql.Dataset
 import org.scalatest.FunSuite
 
-class PopularDestinationsJobTest extends FunSuite with DataFrameSuiteBase {
-  test("simple test") {
+import java.sql.Timestamp
+
+
+class PopularDestinationsJobTest extends FunSuite with DatasetSuiteBase {
+  test("applyFilterByDepartureDate") {
     val sqlCtx = sqlContext
     import sqlCtx.implicits._
 
-    val input1 = sc.parallelize(List(1, 2, 3)).toDF
-    assertDataFrameEquals(input1, input1) // equal
+    val input = List(
+      Booking(
+        timestamp = Timestamp.valueOf("2019-03-17 13:47:26"), uci = "uci1", age = 16, passengerType = "Adt", bookingStatus = "CONFIRMED",
+        operatingAirline = "KL", originAirport = "AMS", destinationAirport = "LIM",
+        departureDate = Timestamp.valueOf("2019-03-20 13:00:00"),
+        arrivalDate = Timestamp.valueOf("2019-03-21 09:00:00"),
+      ),
+      Booking(
+        timestamp = Timestamp.valueOf("2019-04-17 13:47:26"), uci = "uci1", age = 16, passengerType = "Adt", bookingStatus = "CONFIRMED",
+        operatingAirline = "KL", originAirport = "FOO", destinationAirport = "TRF",
+        departureDate = Timestamp.valueOf("2019-04-20 13:00:00"),
+        arrivalDate = Timestamp.valueOf("2019-04-21 09:00:00"),
+      ),
+      Booking(
+        timestamp = Timestamp.valueOf("2019-05-17 13:47:26"), uci = "uci1", age = 16, passengerType = "Adt", bookingStatus = "CONFIRMED",
+        operatingAirline = "KL", originAirport = "BAR", destinationAirport = "BAZ",
+        departureDate = Timestamp.valueOf("2019-05-20 13:00:00"),
+        arrivalDate = Timestamp.valueOf("2019-05-21 09:00:00"),
+      )
+    )
 
-    val input2 = sc.parallelize(List(4, 5, 6)).toDF
-    intercept[org.scalatest.exceptions.TestFailedException] {
-      assertDataFrameEquals(input1, input2) // not equal
-    }
+    val inputDf: Dataset[Booking] = sc.parallelize(input).toDS
+    val outputDf: Dataset[Booking] = sc.parallelize(input.take(2)).toDS
+
+    val startDate = Timestamp.valueOf("2019-03-01 00:00:00")
+    val endDate = Timestamp.valueOf("2019-05-01 00:00:00")
+    assertDatasetEquals(
+      inputDf.transform(PopularDestinationsJob.applyFilterByDepartureDate(startDate, endDate)),
+      outputDf
+    )
   }
 }
